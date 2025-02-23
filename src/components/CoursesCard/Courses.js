9c99f086ac4dec67daf36030afa7e0e807903b4b
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2025-02-23 15:36:21 -0300

    descrição cursos finalizada

--- src/components/CoursesCard/Courses.js
+++ src/components/CoursesCard/Courses.js
@@ -1,58 +1,65 @@
 export const courses = [
     {
-        "title": "Acompanhamento Terapêutico",
+        title: "Acompanhamento Terapêutico",
         description: 'Desenvolva habilidades essenciais para atuar como Acompanhante Terapêutico, oferecendo suporte especializado a pessoas com deficiência, transtornos mentais ou em processo de reabilitação. Um curso prático e acolhedor para quem deseja contribuir para o bem-estar e a autonomia de quem precisa de acompanhamento.Com duração de 4 semanas e carga horária de 20 horas, o curso aborda temas como o papel do Acompanhante Terapêutico, fundamentos teóricos, planejamento de rotinas, intervenções em diferentes contextos, manejo de crises, estudo de casos e ética profissional.',
         hours: '20 horas',
-        investment: 'Até 6x de 866,66 no cartão (R$5.200)',
-        "bgColor": "bg-[#BF7269]",
-        "innerBgColor": "bg-[#BF7269]/50"
+        nvestent: 'Até 6x de 866,66 no cartão (R$5.200)',
+        bgColor: "bg-[#BF7269]",
+        innerBgColor: "bg-[#BF7269]/50",
+        buttonLink: "#"
     },
     {
         description: 'Aprofunde seus conhecimentos na aplicação de ferramentas da Análise do Comportamento voltadas para o TEA, desenvolvendo habilidades práticas para intervir de forma eficaz e baseada em evidências. Ideal para acadêmicos e profissionais de Psicologia e áreas afins que buscam aprimorar sua atuação no acompanhamento de pessoas com Transtorno do Espectro Autista.O curso aborda temas essenciais, como introdução ao TEA, fundamentos da Análise do Comportamento, tipos de comportamento e princípios básicos, comportamento verbal, conceitos e procedimentos da ABA, registro e aplicação de ensino, manejo de comportamento em sala e o papel do aplicador na intervenção.',
         hours: '40 horas',
         investment: 'Até 6x de 866,66 no cartão (R$5.200)',
-        "title": "Aplicador ABA ao TEA - Nível 1",
-        "bgColor": "bg-[#D18D80]",
-        "innerBgColor": "bg-[#D18D80]/50"
+        title: "Aplicador ABA ao TEA - Introdutório",
+        bgColor: "bg-[#D18D80]",
+        innerBgColor: "bg-[#D18D80]/50",
+        buttonLink: "#"
     },
     {
-        description: 'Aprofunde seus conhecimentos teóricos e práticos na Análise Aplicada do Comportamento ao TEA, desenvolvendo competências avançadas para intervenções mais eficazes. Destinado a acadêmicos e profissionais de Psicologia e áreas afins que já concluíram o curso "Aplicador ABA ao TEA – Nível 1".O curso aborda temas como tipos de registros, tabulação de dados, manejo de comportamentos interferentes e aplicação de programas de ensino, além de um bônus especial com treino de habilidades sociais.',
+        description: 'Aprofunde seus conhecimentos teóricos e práticos na Análise Aplicada do Comportamento ao TEA, desenvolvendo competências avançadas para intervenções mais eficazes. Destinado a acadêmicos e profissionais de Psicologia e áreas afins que já concluíram o curso "Aplicador ABA ao TEA – Introdutório".O curso aborda temas como tipos de registros, tabulação de dados, manejo de comportamentos interferentes e aplicação de programas de ensino, além de um bônus especial com treino de habilidades sociais.',
         hours: '40 horas',
         investment: 'Até 6x de 866,66 no cartão (R$5.200)',
-        "title": "Aplicador ABA ao TEA - Nível 2",
-        "bgColor": "bg-[#D5968F]",
-        "innerBgColor": "bg-[#D5968F]/50"
+        title: "Aplicador ABA ao TEA - Avançado",
+        bgColor: "bg-[#D5968F]",
+        innerBgColor: "bg-[#D5968F]/50",
+        buttonLink: "#"
     },
     {
-        description: 'Desenvolva habilidades para apoiar pessoas com deficiência, transtornos mentais e em reabilitação, oferecendo um acompanhamento terapêutico eficaz e sensível.',
+        description: 'Aprimore seus conhecimentos na Análise Aplicada do Comportamento ao TEA de forma intensiva, unindo os conteúdos das formações introdutória e avançada em um único curso. Desenvolva habilidades práticas e teóricas para intervir de maneira eficaz e baseada em evidências, aprofundando-se no manejo de comportamentos, aplicação de programas de ensino e técnicas avançadas da ABA. Ideal para acadêmicos e profissionais de Psicologia e áreas afins que buscam uma formação completa em menos tempo, o curso aborda desde os fundamentos da Análise do Comportamento até estratégias avançadas, incluindo tipos de registros, tabulação de dados, manejo de comportamentos interferentes e treino de habilidades sociais.',
         hours: '40 horas',
         investment: 'Até 6x de 866,66 no cartão (R$5.200)',
-        "title": "Aplicador ABA - Formação intensiva",
-        "bgColor": "bg-[#E2A59B]",
-        "innerBgColor": "bg-[#E2A59B]/50"
+        title: "Aplicador ABA - Formação intensiva",
+        bgColor: "bg-[#E2A59B]",
+        innerBgColor: "bg-[#E2A59B]/50",
+        buttonLink: "#"
     },
     {
         description: 'Aprimore suas habilidades terapêuticas e aprofunde seus conhecimentos nas terapias comportamentais contextuais, desenvolvendo uma prática clínica mais eficaz e baseada em evidências. Destinado exclusivamente a profissionais de Psicologia, o curso oferece um treinamento completo para a atuação qualificada na clínica comportamental.Com duração de 12 meses, as aulas ocorrem às quartas-feiras, das 18h30 às 20h30, abordando temas como a história e filosofia das terapias comportamentais contextuais, princípios da Análise do Comportamento, Terapia de Aceitação e Compromisso, Psicoterapia Analítica Funcional, Terapia Comportamental Dialética, Ativação Comportamental e Habilidades Sociais.',
         hours: ' 96 horas',
         investment: 'Até 6x de 866,66 no cartão (R$5.200)',
-        "title": "Clínica Comportamental",
-        "bgColor": "bg-[#F1B9B0]",
-        "innerBgColor": "bg-[#F1B9B0]/50"
+        title: "Clínica Comportamental",
+        bgColor: "bg-[#F1B9B0]",
+        innerBgColor: "bg-[#F1B9B0]/50",
+        buttonLink: "#"
     },
     {
         description: 'Desenvolva habilidades para apoiar pessoas com deficiência, transtornos mentais e em reabilitação, oferecendo um acompanhamento terapêutico eficaz e sensível.',
         hours: '40 horas',
         investment: 'Até 6x de 866,66 no cartão (R$5.200)',
-        "title": "FACT",
-        "bgColor": "bg-[#F4C6BE]",
-        "innerBgColor": "bg-[#F4C6BE]/50"
+        title: "FACT",
+        bgColor: "bg-[#F4C6BE]",
+        innerBgColor: "bg-[#F4C6BE]/50",
+        buttonLink: "#"
     },
     {
         description: 'Aprimore sua habilidade de leitura e compreensão de textos acadêmicos e científicos em inglês, expandindo seu vocabulário técnico e desenvolvendo estratégias eficazes de interpretação. Um curso ideal para quem precisa do inglês para estudos, trabalho ou provas de qualificação.Com dois encontros semanais às segundas e quartas, das 18h30 às 20h, a carga horária é de 12 horas por mês. O conteúdo inclui vocabulário e estruturas gramaticais essenciais, estratégias de leitura, produção de resumos, preparação para exames e a aplicação prática do inglês instrumental.',
         hours: '12 horas/mês',
         investment: 'Até 6x de 866,66 no cartão (R$5.200)',
-        "title": "Inglês Instrumental",
-        "bgColor": "bg-[#FFD0C7]",
-        "innerBgColor": "bg-[#FFD0C7]/50"
+        title: "Inglês Instrumental",
+        bgColor: "bg-[#FFD0C7]",
+        innerBgColor: "bg-[#FFD0C7]/50",
+        buttonLink: "#"
     }
 ];
